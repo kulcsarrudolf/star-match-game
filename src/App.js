d8b7da44d2cfceb96b3352bd4da8364bd2a0f960
Your Name <you@example.com>
2020-04-10 20:53:13 +0300

    Other Cleanups

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import utils from './utils/Utils'
+
+const PlayNumber = props => (
+  <button className="number" onClick={() => console.log('Num', props.number)}>{props.number}</button>
+);
 
 function App() {
+  // const stars = utils.random(1,9);
+  const [stars, setStars] = useState(utils.random(1, 9));
   return (
     <div className="game">
       <div className="help">
@@ -9,26 +16,14 @@ function App() {
       </div>
       <div className="body">
         <div className="left">
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
+          {utils.range(1, stars).map(starId =>
+            <div key={starId} className="star" />
+          )}
         </div>
         <div className="right">
-          <button className="number">1</button>
-          <button className="number">2</button>
-          <button className="number">3</button>
-          <button className="number">4</button>
-          <button className="number">5</button>
-          <button className="number">6</button>
-          <button className="number">7</button>
-          <button className="number">8</button>
-          <button className="number">9</button>
+          {utils.range(1, 9).map(number =>
+            <PlayNumber key={number} number={number} />
+          )}
         </div>
       </div>
       <div className="timer">Time Remaining: 10</div>
@@ -44,35 +39,6 @@ const colors = {
   candidate: 'deepskyblue',
 };
 
-// Math science
-const utils = {
-  // Sum an array
-  sum: arr => arr.reduce((acc, curr) => acc + curr, 0),
-
-  // create an array of numbers between min and max (edges included)
-  range: (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i),
-
-  // pick a random number between min and max (edges included)
-  random: (min, max) => min + Math.floor(Math.random() * (max - min + 1)),
-
-  // Given an array of numbers and a max...
-  // Pick a random sum (< max) from the set of all available sums in arr
-  randomSumIn: (arr, max) => {
-    const sets = [[]];
-    const sums = [];
-    for (let i = 0; i < arr.length; i++) {
-      for (let j = 0, len = sets.length; j < len; j++) {
-        const candidateSet = sets[j].concat(arr[i]);
-        const candidateSum = utils.sum(candidateSet);
-        if (candidateSum <= max) {
-          sets.push(candidateSet);
-          sums.push(candidateSum);
-        }
-      }
-    }
-    return sums[utils.random(0, sums.length - 1)];
-  },
-};
 
 
 export default App;
