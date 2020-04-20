f640ec02837dfa110ec3e6aaf32164cdb1b66d77
Kulcsar Rudolf <kulcsarrudolf@gmail.com>
2020-04-20 22:32:26 +0300

    Updates

---
+++ precache-manifest.02707bb50e8921165de6a339f57eaac0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e32c5a53c96c852b9c9148ac323829",
+    "url": "/star-match-game/index.html"
+  },
+  {
+    "revision": "87e2bd52281e72397dc1",
+    "url": "/star-match-game/static/css/main.9ae7bb5a.chunk.css"
+  },
+  {
+    "revision": "05ffdaacbe8ca0bdea0e",
+    "url": "/star-match-game/static/js/2.9d0a61ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/star-match-game/static/js/2.9d0a61ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87e2bd52281e72397dc1",
+    "url": "/star-match-game/static/js/main.b6cca704.chunk.js"
+  },
+  {
+    "revision": "2a3f95b1f83a0f7f30d1",
+    "url": "/star-match-game/static/js/runtime-main.b001b03a.js"
+  }
+]);
